45dac4ea227cec2b440bd39a52fc5b76bbe1ba52
abdelhak <abdelhak@gmail.com>
2018-09-26 17:47:23 +0200

    done

---
+++ JS/js.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+		function affiche() {
+		   document.getElementById('welcomeDiv').style.display =
+"block";
+		}
+		function hide() {
+		   document.getElementById('welcomeDiv').style.display =
+"none";
+		}
+</script>
